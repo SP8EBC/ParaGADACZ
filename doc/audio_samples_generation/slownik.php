b69ee4337685224dca573b0e209fe74a342f5dc4
Mateusz Lubecki <sp8ebc@interia.pl>
2023-02-18 19:18:40 +0100

    audio samples extension changed from mp3 to ogg

---
+++ doc/audio_samples_generation/slownik.php
@@ -0,0 +1,139 @@
+<?php
+# URUCHOMIENIE GENEROWANIA W KONSOLI:
+#
+# php index.php
+
+# TYLKO Z TEJ TABLICY GENEROWANE SĄ SAMPLE
+$slownik = array(
+	array('wszystkie stacje wszystkie stacje wszystkie stacje tu','intro'),
+	array('skrzyczne skrzyczne radio', 'intro2'),
+	array('sierra november nine november sierra kilo', 'intro3'),
+	array('kropka', 'kropka'),
+	array('pierwsza', 'pierwsza'),
+	array('druga', 'druga'),
+	array('trzecia', 'trzecia'),
+	array('czwarta', 'czwarta'),
+	array('piąta', 'piata'),
+	array('szósta', 'szosta'),
+	array('siódma', 'siodma'),
+	array('ósma', 'osma'),
+	array('dziewiąta', 'dziewiata'),
+	array('dziesiąta', 'dziesiata'),
+	array('jedynasta', 'jedynasta'),
+	array('dwunasta', 'dwunasta'),
+    array('trzynasta', 'trynasta'),
+    array('czternasta', 'czternasta'),
+    array('pietnasta', 'pietnasta'),
+    array('szesnasta', 'szesnasta'),
+    array('siedemnasta', 'siedemnasta'),
+    array('osiemnasta', 'osiemnasta'),
+    array('dziewietnasta', 'dziewietnasta'),
+    array('dwudziesta', 'dwudziesta'),
+
+    array('godzina', 'godzina'),
+    array('czasu uniwersalna', 'czasu_uniwersalna'),
+    array('czasu lokalnego', 'czasu_lokalnego'),
+
+    array('aktualne warunki pogodowe', 'aktualne_warunki'),
+    array('prognoza pogoda', 'prognoza_pogody'),
+    array('na następne', 'na_nastepne'),
+
+    array('prędkość wiatru', 'predkosc_wiatru'),
+    array('porywy', 'porywy'),
+    array('wiatr', 'wiatr'),
+    array('temperatura', 'temperatura'),
+    array('ciśnienie', 'cisnienie'),
+    array('ciśnienie w regionie', 'cisnienie_w_regionie'),
+    array('wilgotność', 'wilgotnosc'),
+
+    array('północny', 'polnocny'),
+    array('północny północno wschodni', 'polnocny_polnocno_wschodni'),
+    array('północno wschodni', 'polnocno_wschodni'),
+    array('wschodni północno wschodni', 'wschodni_polnocno_wschodni'),
+    array('wschodni', 'wschodni'),
+    array('wschodni południowo wschodni', 'wschodni_poludniowo_wschodni'),
+    array('południowo wschodni', 'poludniowo_wschodni'),
+    array('południowy południowo wschodni', 'poludniowy_poludniowo_wschodni'),
+    array('południowy', 'poludniowy'),
+    array('południowy południowo zachodni', 'poludniowy_poludniowo_zachodni'),
+    array('południowo zachodni', 'poludniowo_zachodni'),
+    array('zachodni południowo zachodni', 'zachodni_poludniowo_zachodni'),
+    array('zachodni', 'zachodni'),
+    array('zachodni północno zachodni', 'zachodni_polnocno_zachodni'),
+    array('północno zachodni', 'polnocno_zachodni'),
+    array('północny północno zachodni', 'polnocny_polnocno_zachodni'),
+
+    array('metr na sekundę', 'metr_na_sekunde'),
+    array('metry na sekundę', 'metry_na_sekunde'),
+    array('metrów na sekundę', 'metrow_na_sekunde'),
+
+    array('węzeł', 'wezel'),
+    array('węzły', 'wezly'),
+    array('węzłów', 'wezlow'),
+
+    array('stopień', 'stopien'),
+    array('stopnie', 'stopnie'),
+    array('stopni', 'stopni'),
+
+    array('godzine', 'godzine'),
+    array('godziny', 'godziny'),
+    array('godzin', 'godzin'),
+
+    array('hektopaskale', 'hektopaskale'),
+    array('procent', 'procent'),
+    array('celsjusza', 'celsjusza'),
+
+    array('tysiąc', 'tysiac'),
+    array('tysiące', 'tysiace'),
+    array('tysięcy', 'tysiecy'),
+
+    array('sto', 'sto'),
+    array('dwieście', 'dwiescie'),
+    array('trzysta', 'trzysta'),
+    array('czterysta', 'czterysta'),
+    array('pięćset', 'piecset'),
+    array('sześćset', 'szescset'),
+    array('siedemset', 'siedemset'),
+    array('osiemset', 'osiemset'),
+    array('dziewięćset', 'dziewiecset'),
+
+    array('dziesięć', 'dziesiec'),
+    array('dwadzieścia', 'dwadziescia'),
+    array('trzydzieści', 'trzydziesci'),
+    array('czterdzieści', 'czterdziesci'),
+    array('pięćdziesiąt', 'piecdziesiat'),
+    array('sześćdziesiąt', 'szescdziesiat'),
+    array('siedemdziesiąt', 'siedemdziesiat'),
+    array('osiemdziesiąt', 'osiemdziesiat'),
+    array('dziewięćdziesiąt', 'dziewiecdziesiat'),
+
+    array('jeden', 'jeden'),
+    array('dwa', 'dwa'),
+    array('trzy', 'trzy'),
+    array('cztery', 'cztery'),
+    array('pięć', 'piec'),
+    array('sześć', 'szesc'),
+    array('siedem', 'siedem'),
+    array('osiem', 'osiem'),
+    array('dziewięć', 'dziewiec'),
+    array('zero', 'zero'),
+
+    array('dwie', 'dwie'),
+    array('dousłyszenia', 'outro'),
+    array('ty skrzyczne radio koniec komunikatu', 'outro2'),
+
+);
+
+
+
+
+
+
+
+# TA TABLICA TO TYLKO PRZECHOWALNIA
+#$slownik_wszystkie = array(
+#
+#    # rozne
+#    array('tu eksperymentalna automatyczna stacja pogodowa'),
+#    array('test', 'test')
+#);
