60c43f95738315e757479e0883c658b2bf17b49d
Mateusz Lubecki <sp8ebc@interia.pl>
2023-07-03 20:06:51 +0200

    workarounds of possible macro collision with cpprestsdk - main

--- src/main.cpp
+++ src/main.cpp
@@ -37,8 +37,13 @@
 #include "InhibitorAndPttControl.h"
 #include "AvalancheWarnings.h"
 
+#pragma push_macro("U")
+#undef U
+// pragma required as a workaround of possible conflict with cpprestsdk.
+// more info here: https://github.com/fmtlib/fmt/issues/3330
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#pragma pop_macro("U")
 
 #include <string_view>
 #include "CurrentConditionsDownloader.h"
