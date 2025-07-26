38145241ec7e6d76400694bc869aa624185caa28
Mateusz Lubecki <sp8ebc@interia.pl>
2025-07-26 21:27:34 +0200

    software version

--- src/main.h
+++ src/main.h
@@ -8,8 +8,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define MAIN_SOFTWARE_VERSION	"0.12"
-#define MAIN_SOFTWARE_DATE		"15-07-2025"
+#define MAIN_SOFTWARE_VERSION	"0.13"
+#define MAIN_SOFTWARE_DATE		"26-07-2025"
 
 //!< Program will close if any current conditions data for configured source isn't find.
 //#define MAIN_FAIL_ON_MISSING_CURRENT_CONDITIONS
